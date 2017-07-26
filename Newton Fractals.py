c09a4161c3037668f2b390f6acf563b337eaa1b6
Michael Bartlett <bartlettmic@gmail.com>
2017-07-26 15:53:27 -0600

    Orders of magnitude automagic

--- Newton Fractals.py
+++ Newton Fractals.py
@@ -2,8 +2,8 @@
 # FB - 201003291
 from PIL import Image
 import math
-imgx = 50
-imgy = 50
+imgx = 300
+imgy = 300
 image = Image.new("HSV", (imgx, imgy))
 
 # drawing area
@@ -17,31 +17,34 @@ h = 1e-6 # step size for numerical derivative
 eps = 1e-3 # max error allowed
 
 # put any complex function here to generate a fractal for it!
+o_end = 10
+o_frames = 50
 start = 0
 stop = math.pi
-frames = 1000
+frames = 100
 
-def f(z,fr):
-    return z*z*1/math.tan(abs(z)-fr)
+def f(z,fr,fr2):
+    return z*z*1/math.tan(abs(z)-fr)-(fr2+2)
     # return z*z*z+math.cos(abs(z))-1
     # math.cos(abs(z)/2*math.pi)
 
 # draw the fractal
-for _i in range(frames):
-    print(_i,"/",frames)
-    # stdout.flush()
-    step = _i*float(stop)/float(frames)
-    for y in range(imgy):
-        zy = y * (yb - ya) / (imgy - 1) + ya
-        for x in range(imgx):
-            zx = x * (xb - xa) / (imgx - 1) + xa
-            z = complex(zx, zy)
-            for i in range(maxIt):
-                # complex numerical derivative
-                dz = (f(z + complex(h, h),step) - f(z,step)) / complex(h, h)
-                z0 = z - f(z,step) / dz # Newton iteration
-                if abs(z0 - z) < eps: # stop when close enough to any root
-                    break
-                z = z0
-            image.putpixel((x, y), (int(float(i)/float(maxIt) * 255.0), 255, 255))
-    image.convert("RGB").save("render/%03d.png" % _i, "PNG")
+for _ord in range(o_frames):
+    o_step = _ord*float(o_end)/float(o_frames)
+    for _i in range(frames):
+        print(_ord,"/",o_frames,"\t-\t",_i,"/",frames)
+        step = _i*float(stop)/float(frames)
+        for y in range(imgy):
+            zy = y * (yb - ya) / (imgy - 1) + ya
+            for x in range(imgx):
+                zx = x * (xb - xa) / (imgx - 1) + xa
+                z = complex(zx, zy)
+                for i in range(maxIt):
+                    # complex numerical derivative
+                    dz = (f(z + complex(h, h),step,o_step) - f(z,step,o_step)) / complex(h, h)
+                    z0 = z - f(z,step,o_step) / dz # Newton iteration
+                    if abs(z0 - z) < eps: # stop when close enough to any root
+                        break
+                    z = z0
+                image.putpixel((x, y), (int(float(i)/float(maxIt) * 255.0), 255, 255))
+        image.convert("RGB").save("render/{0:0>4}-{1:0>4}.png".format(_ord, _i), "PNG")
