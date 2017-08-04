7e384361257b69c8a8e818e5bc15f9924c3b921d
Michael Bartlett <bartlettmic@gmail.com>
2017-08-04 13:19:06 -0600

    Create directory based on formulas used

--- Newton-Fast-Animate.py
+++ Newton-Fast-Animate.py
@@ -1,17 +1,10 @@
-# Newton fractals
-# FB - 201003291
 from PIL import Image
-import math
-import cmath
-#from mpmath import findroot
-from scipy.misc import derivative
+import math, cmath, sys, os, inspect, re, atexit
 
-def truncate(number, digits) -> float:
-    stepper = pow(10.0, digits)
-    return math.trunc(stepper * number) / stepper
 imgx = 100
 imgy = 100
 image = Image.new("HSV", (imgx, imgy))
+
 # drawing area
 xa = -1.0
 xb = 1.0
@@ -21,17 +14,31 @@ yb = 1.0
 maxIt = 40 # max iterations allowed
 h = 1e-6 # step size for numerical derivative
 eps = 1e-2 # max error allowed
-a = -0.5
 
 def f(z,_i):
-    return z**(8*math.sin(_i/math.pi*2))-z**16-_i
+    return z**(2+_i)-z**16-1+cmath.log(abs(2*z**(1+_i)
+    # return z**(8*math.sin(_i/math.pi*2))-z**16-_i
 def df(z,_i):
-    return 8*z**(4*math.sin(_i/math.pi*2))-16*z**15-_i
-    
+    return 8*z**(3+_i)-16*z**15-_i
+    # return 8*z**(4*math.sin(_i/math.pi))-16*z**15-_i
 
-frame=0
-step=0.1
-_i = 0
+# Record the functions used in the directory name
+funcs = []
+for _f in [f, df]:
+    funcs.append(re.split(r'\s+|\n', inspect.getsource(_f))[3].replace("**","^").replace("*",u"\u00D7").replace("/",u"\u00F7").replace("_i","i").replace("cmath.","").replace("math.",""))
+folder = "./render/" + " _ ".join(funcs)
+
+if not os.path.exists(folder):
+    os.makedirs(folder)
+    
+def make_tiff():
+    image.convert("RGB").save(folder+"/_0.tiff", "PNG")
+atexit.register(make_tiff)
+
+start=0
+frame=start
+step=0.01
+_i = 0+start*step
 while True:
     for y in range(imgy):
         zy = y * (yb - ya) / (imgy - 1) + ya
@@ -49,13 +56,16 @@ while True:
                     z0 = z - (f(z,_i) / dz) # Newton iteration
                 except OverflowError:
                     pass
+                except ZeroDivisionError:
+                    i+=1
+                    continue
                 if abs(z0 - z) < eps: # stop when close enough to any root
                     break
                 z = z0
                 i+=1
             shadow = int((float(i)/float(maxIt))**2 * 255.0)
             image.putpixel((x, y), (255-shadow, 255, shadow*2))
-        print(frame,":",round(_i,1),"\t",round(y/imgy*100),"%", end='\r',flush=True)
-    image.convert("RGB").save("render/%04d.png" % frame, "PNG")
+        print(frame,":",round(_i,3),"\t",round(y/imgy*100),"%", end='\r',flush=True)
+    image.convert("RGB").save(folder+"/%04d.png" % frame, "PNG")
     _i+=step
     frame += 1
