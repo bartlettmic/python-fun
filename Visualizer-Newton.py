175c976772ef89be45206c16835534f1492ae51d
Michael Bartlett <bartlettmic@gmail.com>
2017-08-08 15:39:57 -0600

    Now we're cooking

--- Visualizer-Newton.py
+++ Visualizer-Newton.py
@@ -1,5 +1,13 @@
+#!/usr/bin/env python
+from pydub import AudioSegment
 from PIL import Image
 import math, cmath, sys, os, inspect, re, atexit
+
+filename = "./"+sys.argv[1]
+extension = sys.argv[1].split('.')[1]
+
+song = AudioSegment.from_file(filename, format=extension)
+samples = song.get_array_of_samples()
 
 imgx = 100
 imgy = 100
@@ -12,19 +20,11 @@ ya = -1.0
 yb = 1.0
  
 maxIt = 40 # max iterations allowed
-h = 1e-6 # step size for numerical derivative
 eps = 1e-2 # max error allowed
 
-def f(z,_i):
-    # return z**(2+_i)-z**16-1+(cmath.log(abs(z**(1+_i))))
-    # loudness = (-cmath.cos(_i*math.pi)/2 + 0.5)
-    frac, whole = math.modf(_i/2.0+0.5)
-    loudness = 2.0*abs(0.5 - frac)
-    return (9*loudness+1)*z**(3+_i/15.0)-(z**(16+_i/15.0))+(cmath.log(abs(z**(loudness*2.5))))-1
-    # return z**((3))-(z**16)+(cmath.log(abs(2*z**(1+_i)))) - 1
+def f(z,_i, loudness):
+    return (10)*z**(3+_i)-(z**(16))+(cmath.log(abs(z**(1+loudness**2))))-1
 def df(z,_i):
-    frac, whole = math.modf(_i/2.0+0.5)
-    loudness = 2.0*abs(0.5 - frac)
     return (z)**(5)-(16)*z**(15)-1
 
 # Record the functions used in the directory name
@@ -36,30 +36,32 @@ folder = "./render/" + " _ ".join(funcs)
 if not os.path.exists(folder):
     os.makedirs(folder)
     
-def make_tiff():
-    image.convert("RGB").save(folder+"/_0.tiff", "PNG")
-atexit.register(make_tiff)
-
+fps = 60.0
+frames = int(fps*len(song) / 1000.0)
+sampleSize = len(samples)
 frame=0
-step=0.1
-_i = frame*step
-# 11.5 MAX
-while True:
+Sstep = sampleSize/frames
+sample = frame*Sstep
+Mstep = 0.01
+_i = frame*Mstep
+
+print(frames," frames, sample-step size: ",Sstep)
+
+while frame < frames:
+    loud=samples[int(sample)]/song.max
     for y in range(imgy):
         zy = y * (yb - ya) / (imgy - 1) + ya
         for x in range(imgx):
             zx = x * (xb - xa) / (imgx - 1) + xa
-            z = complex(zy, zx)
-            __i = (1-math.cos(_i*(math.pi)))
+            z=complex(zy,zx)
             i=0
             while i < maxIt:
                 try:
                     dz = df(z,_i)
-                    # dz = (f(z + complex(h, h), _i) - f(z, _i)) / complex(h, h)
                 except OverflowError:
                     pass
                 try:
-                    z0 = z - (f(z,_i) / dz) # Newton iteration
+                    z0 = z - (f(z,_i,loud) / dz) # Newton iteration
                 except OverflowError:
                     pass
                 except ZeroDivisionError:
@@ -71,7 +73,9 @@ while True:
                 i+=1
             shadow = int((float(i)/float(maxIt))**2 * 255.0)
             image.putpixel((x, y), (255-shadow, 255, shadow*2))
-        print(frame,":",round(_i,3),"\t",round(y/imgy*100),"%", end='\r',flush=True)
+        print(frame,"/",frames,":",round(_i,3)," ~ ",round(y/imgy*100),"%", end='\r',flush=True)
     image.convert("RGB").save(folder+"/%04d.png" % frame, "PNG")
-    _i+=step
+    sample += Sstep
+    _i+=Mstep
     frame += 1
+image.convert("RGB").save(folder+"/_0.tiff", "PNG")
