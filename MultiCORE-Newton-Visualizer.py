2206973e6f8921c6fa6b1f7a64a6fee6d18849f9
Michael Bartlett <bartlettmic@gmail.com>
2017-08-21 23:56:43 -0600

    ALMOST THERE

--- MultiCORE-Newton-Visualizer.py
+++ MultiCORE-Newton-Visualizer.py
@@ -18,7 +18,7 @@ sampleSize = len(samples)
 def f(z,_i,loudness):
     return abs(z**(2+_i))-z**16-1+cmath.log(abs(z**(4)))
 def df(z,_i,loudness):
-    return 8*z**(3+_i)-z-(16)*z**(15)-(1.5-0.5*loudness)
+    return 8*z**(3+_i)-z-(16)*z**(15)-(2-loudness)
     # return 8*z**(3+_i)-z-(16)*z**(15)-(1+loudness)
 
 # Record the functions used in the directory name
