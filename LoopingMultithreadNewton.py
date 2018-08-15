f85d205c0b6e4d305d4e0aac92472d77a0f7f97a
Michael Bartlett <michael.bartlett@nrel.gov>
2018-08-15 15:39:04 -0600

    nice

--- LoopingMultithreadNewton.py
+++ LoopingMultithreadNewton.py
@@ -122,9 +122,9 @@ def render(start, stop, jobID,q):
                     z = z0
                     i+=1
                 
-                shadow = float(i)/float(maxIt)
+                shadow = float(i)/float(maxIt) * (255*frame/frames)
                 
-                hue = ((1-shadow**(1+loud))*255) - (255*frame/frames)
+                hue = ((1-shadow**(1+loud))*255) 
                 sat = loud*255
                 lum = 255*shadow**(3-(-2*loud))
 
